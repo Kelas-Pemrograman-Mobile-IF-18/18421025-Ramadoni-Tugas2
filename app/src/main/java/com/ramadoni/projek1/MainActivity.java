757b9b8cb2be058f3779ca32a815417de9af6332
ramadoniinformatikaubl <donicyberit@gmail.com>
2021-03-24 23:41:34 +0700

    Tugas2

--- app/src/main/java/com/ramadoni/projek1/MainActivity.java
+++ app/src/main/java/com/ramadoni/projek1/MainActivity.java
@@ -3,18 +3,40 @@ package com.ramadoni.projek1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView txtNama;
+    TextView txtTampil;
+    EditText edtNpm, edtNama, edtAlamat, edtJurusan, edtNoTelpon;
+    Button btnSubmit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        txtNama = (TextView) findViewById(R.id.txtNama);
-        txtNama.setText("Ramadoni");
+        txtTampil = (TextView) findViewById(R.id.txtNPMNamaDanAlamatJurusanNoTelpon);
+        edtNpm = (EditText) findViewById(R.id.edtNpm);
+        edtNama = (EditText) findViewById(R.id.edtNama);
+        edtJurusan = (EditText) findViewById(R.id.edtJurusan);
+        edtAlamat =(EditText) findViewById(R.id.edtAlamat);
+        edtNoTelpon =(EditText) findViewById(R.id.edtNoTelpon);
+        btnSubmit = (Button) findViewById(R.id.btnSubmit);
+
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String strNpm = edtNpm.getText().toString();
+                String strNama = edtNama.getText().toString();
+                String strJurusan = edtJurusan.getText().toString();
+                String strAlamat = edtAlamat.getText().toString();
+                String strNoTelpon = edtNoTelpon.getText().toString();
+                txtTampil.setText(strNpm + "\n" + strNama + "\n" + strAlamat + "\n" + strJurusan + "\n" + strNoTelpon);
+            }
+        });
     }
 }
